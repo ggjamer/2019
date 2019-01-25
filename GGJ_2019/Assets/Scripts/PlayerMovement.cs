01dd56d473bc2a3e70e3918478c35ec0f565cf0c
Yannis Rohloff <yannis@uni-bremen.de>
2019-01-25 21:33:40 +0100

    ok

---
+++ GGJ_2019/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 20;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(
+            Time.deltaTime * speed * Input.GetAxis("Horizontal"),
+            Time.deltaTime * speed * Input.GetAxis("Vertical"),
+            0);
+
+    }
+
+
+}
