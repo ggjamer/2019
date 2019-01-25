5d9372f1097b6c3fb14e1528a15a5e03908385fc
SirrahErydya <kollasch@uni-bremen.de>
2019-01-25 23:54:47 +0100

    Add logic scripts

---
+++ GGJ_2019/Assets/Scripts/Logic/GameLogic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    private static GameLogic _instance;
+    public static GameLogic Instance {
+        get {
+            return _instance;
+        }
+    }
+    
+    private GameState _gameState;
+    
+    void Awake() {
+        _instance = this;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameState = GameState.NEW_IN_TOWN;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(_gameState) {
+        }
+    }
+}
---
+++ GGJ_2019/Assets/Scripts/Logic/GameState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    NEW_IN_TOWN,
+    SCANDAL,
+    MAYORS_PERMISSION,
+    JAIL_VISIT,
+    TOUGHBALL_TALK,
+    COFFEE_BREAK,
+    FINALE
+}
+
