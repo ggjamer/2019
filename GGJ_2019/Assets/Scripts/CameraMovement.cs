4d580adb0c70f12ff9f40b16f4206bebdbd76f65
Luca Nixdorf <nixdorf@uni-bremen.de>
2019-01-26 14:26:26 +0100

    Merge branch 'master' into dialogue_system
    # Conflicts:
    #	GGJ_2019/Assets/Textures.meta

---
+++ GGJ_2019/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+	private Camera _camera;
+	private Vector3 _playerPosition = new Vector3(0, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		_camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+		
+	}
+}
