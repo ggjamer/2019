039de1ee584938eec67c4bad212e0b95d27a63da
Yannis Rohloff <yannis@uni-bremen.de>
2019-01-26 16:04:44 +0100

    menu stuff

---
+++ GGJ_2019/Assets/Scripts/Menu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Experimental.Rendering.LightweightPipeline;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+
+
+    public Button continueButton;
+    public Button saveButton;
+    public Button quitButton;
+    
+    void Start()
+    {
+        continueButton.onClick.AddListener(delegate() { Continue();});
+        saveButton.onClick.AddListener(delegate() { Save();});
+        quitButton.onClick.AddListener(delegate() { Quit();});
+    }
+   
+    public void StartGame(string level)
+    {
+        EventSystem.current.SetSelectedGameObject(continueButton.gameObject);
+    }
+    
+    public void Continue(){
+    }
+    
+    public void Save(){
+    
+    }
+    
+    public void Quit(){
+    
+    }
+}
---
+++ GGJ_2019/Assets/SelectableText.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class SelectableText
+    {
+        public int id;
+        public string text;
+        public GameObject gameObject;
+    }
+}
