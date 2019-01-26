a9eb656e71319e8bc6f1edab2837d84a1fdb4f54
SirrahErydya <kollasch@uni-bremen.de>
2019-01-26 14:16:14 +0100

    Add character creation script

---
+++ GGJ_2019/Assets/Scripts/CharacterCreation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterCreation : MonoBehaviour {
+    
+    public Text nameInput;
+    public Text genderInput;
+
+    private String name;
+    private int gender;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void submit() {
+        
+    }
+}
