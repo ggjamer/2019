41acb5d85efaedf9c1c147b9a6d9a4449ae645da
Robert Stenzel <rstenzel@uni-bremen.de>
2019-01-27 03:00:58 +0100

    music ueberall

---
+++ GGJ_2019/Assets/Scripts/MusicController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+
+	private static MusicController _instance;
+
+	public AudioClip Music;
+
+	private AudioSource _audioSource;
+
+	private void Awake()
+	{
+		if (_instance != null && _instance != this)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			_instance = this;
+		}
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		_audioSource = gameObject.GetComponent<AudioSource>();
+		_audioSource.loop = true;
+		_audioSource.playOnAwake = true;
+		_audioSource.volume = 0.3f;
+
+		_audioSource.clip = Music;
+		_audioSource.Play();
+
+		DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
