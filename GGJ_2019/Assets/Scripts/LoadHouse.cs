f61ace91e64952258c80140a6e39c7f672a0d5fe
Robert Stenzel <rstenzel@uni-bremen.de>
2019-01-27 03:41:53 +0100

    sound beim in ein haus gehen

--- GGJ_2019/Assets/Scripts/LoadHouse.cs
+++ GGJ_2019/Assets/Scripts/LoadHouse.cs
@@ -9,8 +9,32 @@ public class LoadHouse : MonoBehaviour
 	public string SceneName;
 	public Locations location;
 
+	public AudioClip DoorSound;
+	private AudioSource _audioSource;
+
+	private void Start()
+	{
+		_audioSource = gameObject.GetComponent<AudioSource>();
+
+		_audioSource.loop = true;
+		_audioSource.playOnAwake = true;
+		_audioSource.volume = 0.3f;
+
+		_audioSource.clip = DoorSound;
+
+	}
+
 	public void LoadScene() {
+		_audioSource.Play();
+		StartCoroutine( WaitForSound());
+	}
+
+	IEnumerator WaitForSound()
+	{
+		yield return new WaitForSeconds(DoorSound.length * 0.6f);
 		GameLogic.Instance.Location = location;
 		SceneManager.LoadScene(SceneName);
 	}
+
+
 }
